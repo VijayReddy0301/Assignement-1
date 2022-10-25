2c00522722936d3fba85cba23b82ec5e7a26fd9a
VijaReddy0301 <vijaychandra015@gmail.com>
2022-10-25 11:01:29 +0530

    Program 4

---
+++ ProgramFour.java
@@ -0,0 +1,24 @@
+
+public class ProgramFour {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		int n=18;
+		for(int i=0;i<=n;i++) {
+			for(int j=0;j<n;j++) {
+				
+				if((j==0 && i>=n/2) || (j==n-1 && i>=n/2) || (i==n) ||  i-j>=(n)/2 ||  i+j>=n-1+(n/2) ) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			
+			System.out.println();
+		}
+
+	}
+}
+
